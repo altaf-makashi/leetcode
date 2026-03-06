e8bff1dc58ab4aecc401de4eeabf9667067c132f
Altaf1021 <156635336+Altaf1021@users.noreply.github.com>
2026-03-06 16:27:51 +0530

    Time: 0 ms (100.00%) | Memory: 15.5 MB (99.75%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+        int n=nums.size(),index=-1;
+        for(int i=n-1;i>0;i--){
+            if(nums[i]>nums[i-1]){
+                index=i-1;
+                break;
+            }
+        }
+        if(index==-1){
+            reverse(nums.begin(),nums.end());
+            return ;
+        }
+        for(int i=n-1;i>index;i--){
+            if(nums[i]>nums[index]){
+                swap(nums[i],nums[index]);
+                break;
+            }
+        }
+        reverse(nums.begin()+index+1,nums.end());
     }
 };
