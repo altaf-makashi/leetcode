8a2cf118d573d5c661e40143bdb90c292f5967fd
Altaf1021 <156635336+Altaf1021@users.noreply.github.com>
2026-03-09 22:06:38 +0530

    Time: 27 ms (22.78%) | Memory: 17.3 MB (83.02%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         vector<vector<int>>ans;
-        set<vector<int>>st;
+        // set<vector<int>>st;
         sort(nums.begin(),nums.end());
         int n=nums.size();
         for(int i=0;i<n-3;i++){
@@ -11,10 +11,9 @@ public:
                 if(j>i+1 && nums[j]==nums[j-1])continue;
                 int left=j+1,right=n-1;
                 while(left<right){
-                    
                     long long sum=(long long)nums[i]+nums[left]+nums[right]+nums[j];
                     if(sum==target){
-                        st.insert({nums[i],nums[j],nums[left],nums[right]});
+                        ans.push_back({nums[i],nums[j],nums[left],nums[right]});
                         while (left < right && nums[left] == nums[left + 1]) left++;
                         while (left < right && nums[right] == nums[right - 1]) right--;
                         left++;
@@ -29,7 +28,7 @@ public:
                 }
             }
         }
-        for(auto it:st)ans.push_back(it);
+        // for(auto it:st)ans.push_back(it);
         return ans;
     }
 };
