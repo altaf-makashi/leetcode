05e4999b9d3d857d6511a93e9bc15599ee664c93
Altaf Makashi <156635336+altaf-makashi@users.noreply.github.com>
2026-05-02 12:02:05 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (80.79%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
+    int fun(int number,int i,int sign,string &s){
+        if(i>=s.size() || !isdigit(s[i]))return number*sign;
+        int curr_num=s[i]-'0';
+        if(number>(INT_MAX-curr_num)/10){
+            return sign==-1?INT_MIN:INT_MAX;
+        }
+        number=(number*10)+curr_num;
+        return fun(number,i+1,sign,s);
+    }
     int myAtoi(string s) {
-        int i = 0, n = s.length();
-        long result = 0; // Use long to detect overflow easily
-        int sign = 1;
+        int i=0;
+        int n=s.size(),sign=1;
+        while (i < n && s[i] == ' ') {
+            i++;
+        }
 
-        // 1. Skip whitespace
-        while (i < n && s[i] == ' ') i++;
-
-        // 2. Handle sign
+        // ... then process exactly ONE optional sign
         if (i < n && (s[i] == '+' || s[i] == '-')) {
             sign = (s[i] == '-') ? -1 : 1;
             i++;
         }
-
-        // 3. Convert digits and handle overflow
-        while (i < n && isdigit(s[i])) {
-            int digit = s[i] - '0';
-
-            // Check overflow before updating result
-            if (result > (INT_MAX - digit) / 10) {
-                return (sign == 1) ? INT_MAX : INT_MIN;
-            }
-
-            result = result * 10 + digit;
-            i++;
-        }
-
-        return (int)(result * sign);
+        int number=0;
+        return fun(number,i,sign,s);
     }
 };
