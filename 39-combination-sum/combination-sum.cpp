4648e95ef5d9f5be070900b21aa34f3211107fce
Altaf Makashi <156635336+altaf-makashi@users.noreply.github.com>
2026-06-02 11:47:41 +0530

    Time: 7 ms (24.24%) | Memory: 14.3 MB (41.55%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
-vector<vector<int>>ans;
-void fun(int i,int curr_sum,int k,vector<int>temp,vector<int>num){
-    if(i==num.size()){
-        if(curr_sum==k)ans.push_back(temp);
-        return;
-    }
-    // if(curr_sum==k){
-    //     ans.push_back(temp);
-    // }
-    if(curr_sum+num[i]<=k){
-        temp.push_back(num[i]);
-        fun(i,curr_sum+num[i],k,temp,num);
+    vector<vector<int>>ans;
+    void helper(int index,int curr_sum,vector<int>&temp,vector<int>&nums,int target){
+        if(curr_sum>target || index>=nums.size())return;
+        if(curr_sum==target){
+            ans.push_back(temp);
+            return;
+        }
+        temp.push_back(nums[index]);
+        helper(index,curr_sum+nums[index],temp,nums,target);
         temp.pop_back();
+        helper(index+1,curr_sum,temp,nums,target);
     }
-    fun(i+1,curr_sum,k,temp,num);
-}
-
-    vector<vector<int>> combinationSum(vector<int>& num, int k) {
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
         vector<int>temp;
-        fun(0,0,k,temp,num);
+        helper(0,0,temp,nums,target);
         return ans;
     }
 };
