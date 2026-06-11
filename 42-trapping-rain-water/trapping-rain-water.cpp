032ee8fc2f5501d84fb7f878af3d680060ea7d05
Altaf Makashi <156635336+altaf-makashi@users.noreply.github.com>
2026-06-12 00:06:18 +0530

    Time: 1 ms (22.91%) | Memory: 27.4 MB (6.38%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,53 +1,20 @@
 class Solution {
 public :
-    void PGE(vector<int>&pge,vector<int>&nums){
-        int n=nums.size();
-        stack<int>st;
-        st.push(nums[0]);
-        for(int i=1;i<n;i++){
-            while(!st.empty() && st.top()<=nums[i]){
-                st.pop();
-            }
-            if(!st.empty()){
-                pge[i]=st.top();
-            }
-            if (st.empty()) {
-                st.push(nums[i]);
-            } else if (nums[i] < st.top()) {
-                // We only care about tracking the largest boundaries
-                st.push(st.top()); 
-            }
-        }
-    }
-    void NGE(vector<int>&nge,vector<int>&nums){
-        stack<int>st;
-        int n=nums.size();
-        st.push(nums[n-1]);
-        for(int i=n-2;i>=0;i--){
-            while(!st.empty() && st.top()<=nums[i]){
-                st.pop();
-            }
-            if(!st.empty())
-                nge[i]=st.top();
-            if (st.empty()) {
-                st.push(nums[i]);
-            } else if (nums[i] < st.top()) {
-                // We only care about tracking the largest boundaries
-                st.push(st.top()); 
-            }
-        }
-    }
     int trap(vector<int>& nums) {
         int n=nums.size();
-        vector<int>pge(n,-1),nge(n,-1);
-        PGE(pge,nums);
-        NGE(nge,nums);
+        vector<int>pge(n),nge(n);
+        pge[0]=nums[0];
+        for(int i=1;i<n;i++){
+            pge[i]=max(pge[i-1],nums[i]);
+        }
+        nge[n-1]=nums[n-1];
+        for(int i=n-2;i>=0;i--){
+            nge[i]=max(nge[i+1],nums[i]);
+        }
         int ans=0;
         for(int i=0;i<n;i++){
-            if(pge[i]!=-1 && nge[i]!=-1){
-                int min_height=min(pge[i],nge[i]);
-                ans+=(min_height-nums[i]);
-            }
+            if(pge[i]!=-1 && nge[i]!=-1)
+                ans+=min(pge[i],nge[i])-nums[i];
         }
         return ans;
     }
