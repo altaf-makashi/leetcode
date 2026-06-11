9a9880fa048932abe8ab614e646314c6f94afecb
Altaf Makashi <156635336+altaf-makashi@users.noreply.github.com>
2026-06-11 22:07:52 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (37.12%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void NGE(vector<int>&nge,vector<int>&nums2){
+    void NGE(map<int,int>&mpp,vector<int>&nums2){
         stack<int>st;
         int n=nums2.size();
         st.push(nums2[n-1]);
@@ -9,21 +9,19 @@ public:
                 st.pop();
             }
             if(!st.empty())
-                nge[i]=st.top();
+                mpp[nums2[i]]=st.top();
             st.push(nums2[i]);
         }
     }
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         int n2=nums2.size();
         int n1=nums1.size();
-        vector<int>nge(n2,-1),ans(n1,-1);
-        NGE(nge,nums2);
+        vector<int>ans(n1,-1);
+        map<int,int>mpp;
+        NGE(mpp,nums2);
         for(int i=0;i<n1;i++){
-            for(int j=0;j<n2;j++){
-                if(nums1[i]==nums2[j]){
-                    ans[i]=nge[j];
-                    break;
-                }
+            if(mpp.find(nums1[i])!=mpp.end()){
+                ans[i]=mpp[nums1[i]];
             }
         }
         return ans;
