8ae71655c8f4fbd8749dccbce0f2cd1a13fb6708
Altaf Makashi <156635336+altaf-makashi@users.noreply.github.com>
2026-06-21 15:11:31 +0530

    Time: 19 ms (93.94%) | Memory: 94.2 MB (5.92%) - LeetSync

--- 937-online-stock-span/online-stock-span.cpp
+++ 937-online-stock-span/online-stock-span.cpp
@@ -1,26 +1,30 @@
 class StockSpanner {
 public:
-    int i=-1;
-    stack<int>st;
-    vector<int>count,nums;
+    // We only need the stack and a vector to keep track of prices by their index
+    stack<int> st; // Stores indices
+    vector<int> nums;
+    int i;
+
     StockSpanner() {
+        i = -1;
     }
     
     int next(int price) {
         i++;
-        count.push_back(1);
         nums.push_back(price);
-        while(!st.empty() && nums[i]>=nums[st.top()]){
-            count[i]+=count[st.top()];
+        
+        // Pop elements from the stack while the current price is greater than 
+        // or equal to the price at the index on top of the stack
+        while (!st.empty() && price >= nums[st.top()]) {
             st.pop();
         }
+        
+        // If stack is empty, 'price' is greater than all previous prices
+        int span = st.empty() ? (i + 1) : (i - st.top());
+        
+        // Push the current index onto the stack
         st.push(i);
-        return count[i];
+        
+        return span;
     }
 };
-
-/**
- * Your StockSpanner object will be instantiated and called as such:
- * StockSpanner* obj = new StockSpanner();
- * int param_1 = obj->next(price);
- */
