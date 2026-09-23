5036cad0c929c2f5ab664a8d9bee40e8afedb08a
Altaf Makashi <156635336+altaf-makashi@users.noreply.github.com>
2026-09-23 15:13:09 +0530

    Time: 4 ms (8.49%) | Memory: 26 MB (77.56%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,20 +1,25 @@
 class Solution {
-public :
+public:
     int trap(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>pge(n),nge(n);
-        pge[0]=nums[0];
-        for(int i=1;i<n;i++){
-            pge[i]=max(pge[i-1],nums[i]);
-        }
-        nge[n-1]=nums[n-1];
-        for(int i=n-2;i>=0;i--){
-            nge[i]=max(nge[i+1],nums[i]);
-        }
-        int ans=0;
-        for(int i=0;i<n;i++){
-            if(pge[i]!=-1 && nge[i]!=-1)
-                ans+=min(pge[i],nge[i])-nums[i];
+        int n=nums.size(),ans=0;
+        int l=0,r=n-1;
+        int lmax=nums[l],rmax=nums[r];
+        while(l<r){
+            if(nums[l]<nums[r]){
+                if(nums[l]<lmax){
+                    ans+=lmax-nums[l];
+                }else{
+                    lmax=nums[l];
+                }
+                l++;
+            }else{
+                if(nums[r]<rmax){
+                    ans+=rmax-nums[r];
+                }else{
+                    rmax=nums[r];
+                }
+                r--;
+            }
         }
         return ans;
     }
